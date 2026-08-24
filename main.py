3474d288a48827a058860b7918ac8d89cd93e262
Alon Perez <alonperez2012@gmail.com>
2026-08-24 15:53:47 -0700

    Add initial flashcard notes and parsing function

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello World")
+notes = ["Letters in the alphabet : Twenty-six", "Python : An easy to learn programming language"]
+
+def parse_notes(notes):
+        flashcard = []
+        for note in notes:
+                colon_index = note.find(":")
